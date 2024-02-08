74f76e29ddbd52f6ef248e3904813cff43771354
Nuhazet Correa Torres <nuhazet6@gmail.com>
2024-02-08 15:11:51 +0000

    deploy updated

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git commit -m "deploy"
 git push
 ssh nuhazet@nuhazet.arkania.es "
   cd travelroad_spring
-  ./mvnw package
+  ./run.sh
   git pull
   systemctl --user restart travelroad_spring.service
 "
