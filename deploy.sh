a24e3a7da816a0fb6ce6f45836a41e972be5a0d4
Nuhazet Correa Torres <nuhazet6@gmail.com>
2024-02-08 15:20:37 +0000

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd ~/travelroad_spring
+./run.sh
 git add .
 git commit -m "deploy"
 git push
 ssh nuhazet@nuhazet.arkania.es "
   cd travelroad_spring
-  ./run.sh
   git pull
   systemctl --user restart travelroad_spring.service
 "
